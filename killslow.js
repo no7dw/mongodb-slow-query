526cf40c4a6178ce058c1a56585e50805eba3f7d
no7dw <no7david@gmail.com>
2014-12-09 00:26:21 +0800

    first commit

---
+++ killslow.js
@@ -0,0 +1,25 @@
+var j=0;
+var _DEF_TIME=10; //设置慢查询时间
+for (var i in db.currentOP().inprog) {
+    var op = "";
+    var opid;
+    var memProg={}
+    if (typeof(undefined) == typeof(db.currentOP().inprog[i])) {
+        continue;
+    }
+    memProg=db.currentOP().inprog[i];
+    op = memProg.op;
+    opid = memProg.opid;
+    print(i);
+    if (op=="query") {
+        if (memProg.hasOwnProperty('secs_running')) {
+            var useTime  = memProg.secs_running;
+            if (useTime >= _DEF_TIME) {
+                db.killOp(opid);
+                j++;
+                print("killed "+j+" Query Operation!");
+            }
+        }
+    }
+}
+//./mongo 10.15.107.154:30000 < ./killslow.js
